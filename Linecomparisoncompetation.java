284869608ed1caa794f078725939aec165d2f0d6
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-22 08:26:20 +0530

    Add:adding line comaprison program

---
+++ Linecomparisoncompetation.java
@@ -0,0 +1,8 @@
+package LineComparison;
+
+public class Linecomparisoncompetation {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the line comparison computation");
+    }
+}
